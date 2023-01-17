9df08d534f555b598796a609316ea0148af89670
Alexander Kuermeier <alexander.kuermeier@web.de>
2023-01-17 20:47:11 +0100

    changed db connection

--- skicourse_signup/app.py
+++ skicourse_signup/app.py
@@ -3,12 +3,12 @@ import pandas as  pd
 import psycopg2
 import os
 
-from functions import test
+## Need a Setup.py for deploying with functions.py
 
 app = Flask(__name__)
 # Connect to your postgres DB
-conn = psycopg2.connect(database="database",
-                    host="app-e7eb027c-bde8-450a-8cc0-a27b07a097a2-do-user-13321347-0.b.db.ondigitalocean.com",
+conn = psycopg2.connect(database="db",
+                    host="app-a0e7425b-c058-4fa2-bd02-410e7c008f2c-do-user-13321347-0.b.db.ondigitalocean.com",
                     user=os.getenv('DB_USERNAME'),
                     password=os.getenv('DB_PASSWORD'),
                     port="25060")
