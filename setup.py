03bb6acce1aed1c6223ddb809923f7dd0c0c6bdd
Alexander Kuermeier <alexander.kuermeier@web.de>
2023-01-17 21:09:50 +0100

    trying build with setup py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skicourse_signup',
+    version='0.0.17',
+    description='Testing',
+    author='Alexander Kuermeier',
+    author_email='alexander.kuermeier@web.de',
+    packages=find_packages()
+)
--- skicourse_signup/app.py
+++ skicourse_signup/app.py
@@ -7,22 +7,22 @@ import os
 
 app = Flask(__name__)
 # Connect to your postgres DB
-conn = psycopg2.connect(database="db",
-                    host="app-a0e7425b-c058-4fa2-bd02-410e7c008f2c-do-user-13321347-0.b.db.ondigitalocean.com",
-                    user=os.getenv('DB_USERNAME'),
-                    password=os.getenv('DB_PASSWORD'),
-                    port="25060")
+#conn = psycopg2.connect(database="db",
+#                    host="app-a0e7425b-c058-4fa2-bd02-410e7c008f2c-do-user-13321347-0.b.db.ondigitalocean.com",
+#                    user=os.getenv('DB_USERNAME'),
+#                    password=os.getenv('DB_PASSWORD'),
+#                    port="25060")
 
-@app.route('/who', methods=['GET'])
-def who():
-    name = request.args.get('name')
-    name = 'Alexander' if name is None else name
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
-    cur.execute(f"INSERT INTO testtable (spalte_1) VALUES ('{name} has the highground')")
-    conn.commit()
-    cur.close()
-    return f"<p>{name} has the highground</p>"
+#@app.route('/who', methods=['GET'])
+#def who():
+#    name = request.args.get('name')
+ #   name = 'Alexander' if name is None else name
+ #   # Open a cursor to perform database operations
+ #   cur = conn.cursor()
+  #  cur.execute(f"INSERT INTO testtable (spalte_1) VALUES ('{name} has the highground')")
+#    conn.commit()
+#    cur.close()
+#    return f"<p>{name} has the highground</p>"
 
 @app.route("/first_test", methods=['GET'])
 def first_test():
