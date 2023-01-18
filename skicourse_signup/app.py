2893145cb37ad6d871510013b2fc0f4254dd0aff
Alexander Kuermeier <alexander.kuermeier@web.de>
2023-01-18 20:30:54 +0100

    use gmail for mails

--- skicourse_signup/app.py
+++ skicourse_signup/app.py
@@ -6,7 +6,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.web.de'
+# Config Mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
